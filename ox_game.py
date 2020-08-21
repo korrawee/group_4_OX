902af11cccf081475ecf22d0af47a031ffb45322
Pannawat Kingkaew <s6201012620163@email.kmutnb.ac.th>
2020-08-21 23:19:09 +0700

    textinput

--- ox_game.py
+++ ox_game.py
@@ -38,19 +38,45 @@ class Printer:
     def show(self):
         for i in range(3):
             for j in range(3):
-                print("|",end=' ')
+                print("|",end='')
                 tmp = self.board.getChar(i,j) #สร้างตัวแปรไว้เก็บค่าชั่วคราว
-                print(tmp,end = " ")
-                print("|\n-------")
-                self.textInput.getInput(self)
+                print(tmp,end = "")
+            print("|\n-------")
+        self.textInput.getInput(self)
         self.show()
     def resetBoard(self):
         for a in range(3):
             for b in range(3):
                 self.board.setChar(" ", a, b)
 
-class TextInput:
-    def __init__(self):
+class TextInput() :
+    def __init__(self,):
+        self.row = 0
+        self.column = 0
+        self.player = 1
+    def getInput(self,obj):
+        self.row = int(input("row: "))
+        self.column = int(input("column: "))   
+        if self.error_check(obj,self.row,self.column) is True :
+            if(self.player == 1):
+                obj.board.setChar("o", self.row, self.column)
+                self.player = 2
+            elif(self.player == 2):
+                obj.board.setChar("x", self.row, self.column)
+                self.player = 1
+    def error_check(self,obj ,row ,column):
+        if(self.row > 2):
+            return False
+        elif(self.column > 2):
+            return False
+        tmp = obj.board.getChar(self.row, self.column)
+        if(tmp == 'o'):
+            return False
+        elif(tmp == 'x'):
+            return False
+        else:
+            return True
+
 
         
 game = Printer()
