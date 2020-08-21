b6b6a6adffdaa309c238e32e30905630ddd2a6c4
PosEnough <wavemaza555@gmail.com>
2020-08-21 23:05:29 +0700

    Fix show() and resetBoard()

--- ox_game.py
+++ ox_game.py
@@ -3,24 +3,22 @@ class Board :
         self.array = [[' ',' ',' '], 
                     [' ', ' ', ' '], 
                     [' ', ' ', ' ']]
-        self.playerTurn = 1
-        self.player = ['O', 'X']
         self.row = 0
         self.column = 0
 
     def win_check(self,):
         state = 0
         for row in range(3):
-            if self.array[row][0] == self.array[row][1] == self.array[row][2] != "_":
+            if self.array[row][0] == self.array[row][1] == self.array[row][2] != " ":
                 state = 1
         # check column        
         for column in range(3):
-            if self.array[0][column] == self.array[1][column] == self.array[2][column] != "_":
+            if self.array[0][column] == self.array[1][column] == self.array[2][column] != " ":
                 state = 1
         # check cross    
-        if self.array[0][0] == self.array[1][1] == self.array[2][2] != "_":
+        if self.array[0][0] == self.array[1][1] == self.array[2][2] != " ":
             state = 1
-        elif self.array[0][2] == self.array[1][1] == self.array[2][0] != "_":
+        elif self.array[0][2] == self.array[1][1] == self.array[2][0] != " ":
             #print(array[0][2], "player is WIN!!!")
             state  = 1
         if(state == 1):
@@ -35,22 +33,21 @@ class Board :
         self.array[r][c] = char
 class Printer:
     def __init__(self,):
-	self.board = Board()
-	self.textInput = TextInput()
+        self.board = Board()
+        self.textInput = TextInput()
     def show(self):
-	for i in range(3):
-	     for j in range(3):
-		print("|",end=' ')
-		tmp = self.board.getChar(i,j) #สร้างตัวแปรไว้เก็บค่าชั่วคราว
-		print(tmp,end = " ")
-	     print("|\n-------")
-        self.textInput.getInput(self)
-	self.show()
+        for i in range(3):
+            for j in range(3):
+                print("|",end=' ')
+                tmp = self.board.getChar(i,j) #สร้างตัวแปรไว้เก็บค่าชั่วคราว
+                print(tmp,end = " ")
+                print("|\n-------")
+                self.textInput.getInput(self)
+        self.show()
     def resetBoard(self):
-	for a in range(3)d:
-	   for b in range(3):
-		self.board.getChar(a,b)
-	self.show()
+        for a in range(3):
+            for b in range(3):
+                self.board.setChar(" ", a, b)
 
 class TextInput:
     def __init__(self):
